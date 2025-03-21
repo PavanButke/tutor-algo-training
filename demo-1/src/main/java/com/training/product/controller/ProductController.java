6419b10b16e853d911ed7fad45691e89d6e9acfe
PavanButke <butkepavan55@gmail.com>
2025-03-21 15:55:06 +0530

    spring boot rest api

---
+++ demo-1/src/main/java/com/training/product/controller/ProductController.java
@@ -0,0 +1,38 @@
+package com.training.product.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.training.product.model.ProductDto;
+
+
+@RestController
+@RequestMapping("/app")
+public class ProductController {
+
+	List<ProductDto> dtos = new ArrayList<>();
+	
+	
+	public ProductController()
+	{
+		dtos.add(new ProductDto(1, "Chair" , 4000 , "Infra"));
+		dtos.add(new ProductDto(2, "Table" , 5000 , "Infra"));
+		dtos.add(new ProductDto(3, "Fan" , 500 , "Electronics"));
+		
+	}
+	
+	@GetMapping(value="/products" )
+	public List<ProductDto> getProducts()
+	{
+		return dtos;
+		
+	}
+	
+	
+}
