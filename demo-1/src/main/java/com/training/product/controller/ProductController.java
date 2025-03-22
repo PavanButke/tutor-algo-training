babcda3c6dd0184045315f5780bdd9f21b2f824c
PavanButke <butkepavan55@gmail.com>
2025-03-22 10:52:53 +0530

    GET PUT POST apis are done

--- demo-1/src/main/java/com/training/product/controller/ProductController.java
+++ demo-1/src/main/java/com/training/product/controller/ProductController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +36,32 @@ public class ProductController {
 		
 	}
 	
+	@GetMapping(value="/product/{productId}")
+	public ProductDto getProductById(@PathVariable("productId") int productId)
+	{
+		for(ProductDto product: dtos)
+		{
+			if(product.getProductId()== productId)
+			{
+				return product;
+			}
+		}
+		
+		return null;
+		
+	}
 	
+	public List<ProductDto> updateProduct(@PathVariable("productId") int productId, @RequestBody ProductDto product) {    
+
+	    ProductDto updateProduct = getProductById(productId);
+	    if (updateProduct != null) { 
+	        updateProduct.setProductName(product.getProductName());
+	        updateProduct.setProductPrice(product.getProductPrice());
+	        updateProduct.setProductCatagory(product.getProductCatagory());
+	        return dtos;
+	        
+	    }
+		return null;
+	    
+	}
 }
